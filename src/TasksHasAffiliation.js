a2ba55bd3e78fa2c90a5741c1ef9d353e114889f
math-miki <math.micky.dtl@gmail.com>
2017-12-06 23:51:21 +0900

    updated to send props

--- src/TasksHasAffiliation.js
+++ src/TasksHasAffiliation.js
@@ -5,13 +5,23 @@ class TasksHasAffiliation extends Component {
   constructor() {
     super();
     this.state = {
-      taskTitle: "宿題"
+      tasks: [["前半","あいうえお", "かきくけこ", "さしすせそ", "たちつてと", "なにぬねの"],["後半", "はひふへほ", "まみむめも", "やゆよ", "らりるれろ", "わをん"]]
     }
   }
   render() {
+    var list = []
+    if(this.state.tasks.length >1) {
+      for(var task in tasks) {
+        if(task.length>1) {
+        list.push(<TaskList taskTitle={this.state.tasks[0]} tasks={list.slice(1, tasks.length) /})
+        } else {
+          list.push(<TaskList taskTitle={this.state.tasks[0]} tasks={[]} /)
+        }
+      }
+    }
     return (
       <div className="tasksHasAffiliation">
-        <TaskList />
+        {list}
       </div>
     )
   }
