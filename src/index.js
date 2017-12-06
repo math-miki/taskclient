861ac75d9cd21c872ad5e2fbe65d59b183650569
math-miki <math.micky.dtl@gmail.com>
2017-12-06 23:54:58 +0900

    updated to test

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import './index.css';
 import App from './App';
 import QrButton from './QrButton';
 import TaskList from './TaskList'
+import TasksWithNoAffiliation from './TasksWithNoAffiliation'
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(<QrButton />, document.getElementById('QRButton'))
-ReactDOM.render(<TaskList />, document.getElementById('tasklist'))
+ReactDOM.render(<TasksWithNoAffiliation />, document.getElementById('tasksWithNoAffiliation'))
 registerServiceWorker();
