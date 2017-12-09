8be882492197a210d11a4e586ec052f6701325d7
math-miki <math.micky.dtl@gmail.com>
2017-12-10 02:35:11 +0900

    transmission with socket.io

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,10 @@
 import io from 'socket.io-client'
+import Action from './Action'
 
 let socket = io.connect("http://localhost:3001")
+
+socket.on("receiveCode", (codeValue) => {
+  Action.receiveCode(codeValue)
+})
 
 export default socket
